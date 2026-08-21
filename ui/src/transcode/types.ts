b1b0e07fe64ee0dbe60788ff5dd22825f66d36c3
TAE-OK-11 <tae.ok.pen@gmail.com>
2026-08-21 21:55:13 +0900

    refactor(ui): add typed transcoding contracts

---
+++ ui/src/transcode/types.ts
@@ -0,0 +1,47 @@
+export type AudioCodec =
+  | 'mp3'
+  | 'opus'
+  | 'vorbis'
+  | 'flac'
+  | 'wav'
+  | 'alac'
+  | 'aac'
+
+export interface DirectPlayProfile {
+  containers: string[]
+  audioCodecs: AudioCodec[]
+  protocols: string[]
+}
+
+export interface TranscodingProfile {
+  container: string
+  audioCodec: AudioCodec
+  protocol: string
+}
+
+export interface BrowserProfile {
+  name: string
+  platform: string
+  directPlayProfiles: DirectPlayProfile[]
+  transcodingProfiles: TranscodingProfile[]
+  codecProfiles: unknown[]
+}
+
+export interface SourceStream {
+  codec?: string
+  container?: string
+  [key: string]: unknown
+}
+
+export interface TranscodeDecision {
+  canDirectPlay?: boolean
+  canTranscode?: boolean
+  transcodeParams?: string
+  sourceStream?: SourceStream
+  [key: string]: unknown
+}
+
+export type FetchTranscodeDecision = (
+  songId: string,
+  browserProfile: BrowserProfile,
+) => Promise<TranscodeDecision | null>
